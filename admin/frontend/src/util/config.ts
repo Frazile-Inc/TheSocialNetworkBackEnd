65d41b85af8ee72af30437ead37908741500ae16
riteshsingh633 <singh.riteshkumar0633@gmail.com>
2026-04-08 17:56:25 +0530

    added docker

--- admin/frontend/src/util/config.ts
+++ admin/frontend/src/util/config.ts
@@ -1,4 +1,3 @@
-export const baseURL: string = "";
-export const secretKey: string = "";
-export const projectName: string = "";
-
+export const baseURL: string = process.env.NEXT_PUBLIC_BASE_URL || "";
+export const secretKey: string = process.env.NEXT_PUBLIC_SECRET_KEY || "";
+export const projectName: string = process.env.NEXT_PUBLIC_PROJECT_NAME || "";
