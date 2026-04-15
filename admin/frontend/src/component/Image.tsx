0881fd674f29376b7948cec9267b2e964f121ac1
Frazile Inc. <frazilex@gmail.com>
2026-04-16 00:41:37 +0530

    files updated

--- admin/frontend/src/component/Image.tsx
+++
@@ -1,2 +0,0 @@
-export  const NoImageUser = "../assets/images/user.png";
-export const NoImage = import("../assets/images/noImage.png");
--- admin/frontend/src/util/config.ts
+++
@@ -1,3 +0,0 @@
-export const baseURL: string = process.env.NEXT_PUBLIC_BASE_URL || "";
-export const secretKey: string = process.env.NEXT_PUBLIC_SECRET_KEY || "";
-export const projectName: string = process.env.NEXT_PUBLIC_PROJECT_NAME || "";
